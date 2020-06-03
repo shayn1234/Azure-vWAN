7cc459f1329fee88f7aae8bc18149a266126b6f2
shayn1234 <shaynaeh.80@gmail.com>
2020-06-04 00:08:23 +0300

    Azure vWAN ARM blueprint - Adding Jinja template

--- scripts/configure-connections.py
+++ scripts/configure-connections.py
@@ -6,11 +6,11 @@ from cloudify import ctx
 from cloudify.state import ctx_parameters as inputs
 
 def main():
-    ctx.logger.info('Configuring vpngateway connections')
+    ctx.logger.info("Configuring vpngateway connections")
     template = Template(ctx.get_resource(inputs['template_file']))
     with tempfile.NamedTemporaryFile(delete=False) as temp_config:
         temp_config.write(template.render(connections=inputs['connection_list']))
-
+    ctx.logger.info("Rendered file name is' {0}\n".format(temp_config.name))
     ctx.instance.runtime_properties['ARMfile'] = temp_config.name
 
 if __name__ == "__main__":
